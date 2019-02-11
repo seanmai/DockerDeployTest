c96a1b932380f020da6f7fa2860150e18cac319f
Sean Mai <seandmai@gmail.com>
2019-02-11 02:19:05 -0700

    Changed p app.js

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to reload. Testing
+            Edit <code>src/App.js</code> and save to reload. Testing feature branch
           </p>
           <a
             className="App-link"
